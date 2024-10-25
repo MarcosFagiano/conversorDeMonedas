e405c0a76126974bc6cabb40503ebdc7bce43753
MarcosFagiano <2112752@ucc.edu.ar>
2024-10-25 10:00:46 -0300

    first commit

---
+++ src/com/alura/conversor/main/Main.java
@@ -0,0 +1,15 @@
+package com.alura.conversor.main;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main  {
+    public static void main(String[] args) throws IOException, Exception {
+        Scanner kScanner = new Scanner(System.in);
+        FileWriter file = new FileWriter("history.txt");
+
+        System.out.println("Hi bitch... Lets do it!!");
+    }
+
+}
---
+++ src/com/alura/conversor/routes/Api.java
@@ -0,0 +1,23 @@
+package com.alura.conversor.routes;
+
+import java.util.Scanner;
+
+/**
+ * api
+ */
+public class Api {
+
+    private static String API_KEY = "573b55dd6c49c58ab606c2fc";
+
+    public static void main(String[] args) {
+        System.out.println(API_KEY);
+    }
+
+    public static String ApiRequest(Scanner kScanner){
+
+
+
+        return "";
+    }
+
+}
